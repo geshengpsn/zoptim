4af96af16b466ab44fd47e7da3b59b73b23ec85b
gesheng <gesheng199832@126.com>
2026-09-10 15:56:43 +0800

    Refactor zla dependency configuration by removing unnecessary optimization settings

--- build.zig
+++ build.zig
@@ -16,10 +16,7 @@ pub fn build(b: *std.Build) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
-    const zla_dep = b.dependency("zla", .{
-        .target = target,
-        .optimize = optimize,
-    });
+    const zla_dep = b.dependency("zla", .{});
     const zla_mod = zla_dep.module("zla");
     // It's also possible to define more custom flags to toggle optional features
     // of this build script using `b.option()`. All defined flags (including
